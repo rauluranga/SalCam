d54abdf8b56314cbfd5176735cfe44ec15658476
Raúl Uranga <ruranga@gmail.com>
2015-04-16 12:11:12 -0500

    Initial Commit

---
+++ SpeedCameraLocatorTests/SpeedCameraLocatorTests.swift
@@ -0,0 +1,36 @@
+//
+//  SpeedCameraLocatorTests.swift
+//  SpeedCameraLocatorTests
+//
+//  Created by Raul on 4/16/15.
+//  Copyright (c) 2015 rauluranga. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class SpeedCameraLocatorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
