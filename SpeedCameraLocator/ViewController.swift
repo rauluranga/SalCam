56446a34187ef80c556f7eecdd64b8e2040299df
Raúl Uranga <ruranga@gmail.com>
2015-04-16 14:47:43 -0500

    added layout for tracking button

--- SpeedCameraLocator/ViewController.swift
+++ SpeedCameraLocator/ViewController.swift
@@ -8,11 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
     
     @IBOutlet weak var mapView: GMSMapView!
+    @IBOutlet weak var tracking_btn: UIButton!
     
     var isTracking: Bool = false;
+    
+    let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,9 +24,9 @@ class ViewController: UIViewController {
         var camera = GMSCameraPosition.cameraWithLatitude(25.433353,longitude:-101.002808, zoom:13)
         
         mapView.camera = camera;
-        //var mapView = GMSMapView.mapWithFrame(CGRectZero, camera:camera)
         
-        mapView.myLocationEnabled = true;
+        let labelHeight = tracking_btn.intrinsicContentSize().height
+        mapView.padding = UIEdgeInsets(top: self.topLayoutGuide.length, left: 0, bottom: labelHeight, right: 0)
         
         var marker = GMSMarker()
         marker.position = camera.target
@@ -31,8 +34,12 @@ class ViewController: UIViewController {
         marker.appearAnimation = kGMSMarkerAnimationPop
         marker.map = mapView
         
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization()
+        
     }
     
+    //MARK: toogle tracking
     
     @IBAction func toogleTracking(sender: UIButton) {
         isTracking = !isTracking;
@@ -44,7 +51,24 @@ class ViewController: UIViewController {
         
     }
     
+    //MARK: CLLocation Manager
     
+    func locationManager(manager: CLLocationManager!, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        if status == .AuthorizedWhenInUse {
+            locationManager.startUpdatingLocation()
+            mapView.myLocationEnabled = true
+            mapView.settings.myLocationButton = true
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        if let location = locations.first as? CLLocation {
+            mapView.camera = GMSCameraPosition(target: location.coordinate, zoom: 13, bearing: 0, viewingAngle: 0)
+            locationManager.stopUpdatingLocation()
+        }
+    }
+    
+    //MARK: Memory handler
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
